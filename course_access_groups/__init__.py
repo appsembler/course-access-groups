6b42247fce7a5a62eaa3ecc1d498e1722992cd75
Omar Al-Ithawi <i@omardo.com>
2021-07-14 15:47:55 +0300

    bump version to 0.5.dev1

--- course_access_groups/__init__.py
+++ course_access_groups/__init__.py
@@ -3,6 +3,6 @@ An Open edX plugin to customize courses access by grouping learners and assignin
 """
 
 
-__version__ = '0.4.0'
+__version__ = '0.5.dev1'
 
 default_app_config = 'course_access_groups.apps.CourseAccessGroupsConfig'
