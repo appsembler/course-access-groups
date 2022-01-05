6a040cd9246e98ed0fb9d12783c148ee88d76b60
Omar Al-Ithawi <i@omardo.com>
2022-01-05 11:55:40 +0300

    RED-2176 fix MembershipRule for SSO learners
    Changes
    =======
    
     - Listen to REGISTER_USER signal in the LMS
     - `Membership.create_from_rules` will be executed twice for SSO users,
       added tests to ensure it won't fail due to duplicate signals
    
    Related Changes
    ===============
    
     - Ensure `UserOrganizationMapping` is added before
       the REGISTER_USER signal is sent in the edx-platform repo

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -6,7 +6,10 @@ Tests for signal handlers.
 import pytest
 
 from course_access_groups.models import Membership
-from course_access_groups.signals import on_learner_account_activated
+from course_access_groups.signals import (
+    on_learner_account_activated,
+    on_learner_register,
+)
 
 from test_utils.factories import (
     MembershipRuleFactory,
@@ -16,9 +19,13 @@ from test_utils.factories import (
 
 
 @pytest.mark.django_db
-def test_working_on_account_activated_signal():
+@pytest.mark.parametrize('receiver_function', [
+    on_learner_account_activated,
+    on_learner_register,
+])
+def test_working_on_account_activated_signal(receiver_function):
     """
-    Ensure USER_ACCOUNT_ACTIVATED is processed correctly.
+    Ensure USER_ACCOUNT_ACTIVATED and REGISTER_USER signals are processed correctly.
     """
     rule = MembershipRuleFactory(domain='example.com')
     mapping = UserOrganizationMappingFactory.create(
@@ -26,22 +33,28 @@ def test_working_on_account_activated_signal():
         organization=rule.group.organization,
     )
 
-    on_learner_account_activated(object(), mapping.user)
+    receiver_function(object(), mapping.user)
     assert Membership.objects.filter(user=mapping.user).exists(), 'Should create the rule'
+
+    receiver_function(object(), mapping.user)  # Should not fail when receiving the signal twice
 
 
 @pytest.mark.django_db
-def test_failed_on_account_activated_signal(monkeypatch, caplog):
+@pytest.mark.parametrize('receiver_function,signal_name', [
+    [on_learner_account_activated, 'USER_ACCOUNT_ACTIVATED'],
+    [on_learner_register, 'REGISTER_USER'],
+])
+def test_failed_on_account_activated_signal(monkeypatch, caplog, receiver_function, signal_name):
     """
-    Ensure USER_ACCOUNT_ACTIVATED errors are logged.
+    Ensure  errors in USER_ACCOUNT_ACTIVATED and REGISTER_USER are logged.
     """
-    monkeypatch.delattr(Membership, 'create_from_rules')  # Act as if the create from rules don't work!
+    monkeypatch.delattr(Membership, 'create_from_rules')  # Act as if create_from_rules() don't work!
 
     user = UserFactory.create(email='someone@example.com')
     MembershipRuleFactory(domain='example.com')
 
     with pytest.raises(AttributeError):
-        on_learner_account_activated(object(), user)
-    assert 'Error receiving USER_ACCOUNT_ACTIVATED signal for user' in caplog.text
+        receiver_function(object(), user)
+    assert 'Error receiving {signal_name} signal for user'.format(signal_name=signal_name) in caplog.text
     assert 'someone@example.com' in caplog.text
     assert 'AttributeError' in caplog.text
